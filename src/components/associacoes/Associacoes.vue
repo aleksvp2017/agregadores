5dfa8698d0df2fcb5ade18e8b8f21337a8c5eac8
Aleksander Pascoal <aleksander.pascoal@planejamento.gov.br>
2019-03-25 17:14:01 -0300

    Criando tela de associacoes, preparando para rota, melhorando cadastro

---
+++ src/components/associacoes/Associacoes.vue
@@ -0,0 +1,89 @@
+<template>
+    <div>
+        <h1 class="tituloAgregador">Associações</h1>
+        <br/>
+
+        <b-card
+            title=""
+            align=""
+            style="background: #d9e6f2"
+            border-variant="secondary"
+            class="mb-2">
+            <b-form-select v-model="orgaoSelecionado" :options="orgaos" size="sm" class="mt-3" />
+            <b-form-select v-model="agregadorSelecionado" :options="opcoesAgregadores" size="sm" class="mt-3" />
+        </b-card>
+        <br/>
+        <br/>
+
+        <div>
+            <b-table id="tabelaAssociacoes" striped hover :items="associacoes" small
+                :per-page="perPage" :current-page="currentPage"
+                :sort-by.sync="sortBy" :sort-desc.sync="sortDesc" :fields="fields">
+                <template slot="agregador" slot-scope="data">
+                    <b-form-select v-model="data.item.agregador" :options="opcoesAgregadores" size="sm" class="mt-3" />                        
+                </template>
+            </b-table>
+            <b-pagination
+                v-show="rows > perPage"
+                v-model="currentPage"
+                :total-rows="rows"
+                :per-page="perPage"
+                aria-controls="tabelaAssociacoes"
+                />        
+        </div>
+
+
+    </div>    
+</template>
+
+<script>
+import { obterOpcoesOrgaos } from '../orgaos/DadosOrgaos.js';
+import { obterOpcoesAgregadores } from '../agregadores/DadosAgregadores.js';
+import { obterTodasAssociacoes } from '../associacoes/DadosAssociacoes.js';
+
+export default {
+    data(){
+        return {
+            orgaoSelecionado: null,
+            orgaos: obterOpcoesOrgaos(),
+            agregadorSelecionado: null,
+            opcoesAgregadores: obterOpcoesAgregadores(),
+            //dados para tabela
+            todasAssociacoes: obterTodasAssociacoes(),
+            currentPage: 1,
+            perPage: 10,
+            sortBy: 'orgao, acao, po',
+            sortDesc: false,
+            fields: [
+                    { key: 'orgao', label:'Órgão', sortable: true },
+                    { key: 'uo', label:'Unidade Orçamentária', sortable: true },
+                    { key: 'acao', label: 'Ação', sortable: true },
+                    { key: 'po', label: 'Plano Orçamentário', sortable: true },
+                    { key: 'agregador', label: 'Agregador'}]            
+        }
+    },
+    computed: {
+        associacoes(){
+            var associacoesFiltradas = this.todasAssociacoes;
+            if (this.orgaoSelecionado){
+                let exp = new RegExp(this.orgaoSelecionado.trim(), 'i');        
+                associacoesFiltradas = associacoesFiltradas.filter(associacao => exp.test(associacao.orgao));                                
+            }
+            if (this.agregadorSelecionado){
+                let exp = new RegExp(this.agregadorSelecionado.trim(), 'i');        
+                associacoesFiltradas = associacoesFiltradas.filter(associacao => exp.test(associacao.agregador));                                
+            }            
+            return associacoesFiltradas;
+        },
+        rows(){
+           return this.associacoes.length;
+        }
+
+    }
+}
+</script>
+
+<style>
+
+</style>
+
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import Agregadores from './components/agregadores/Agregadores.vue';
+import Home from './components/home/Home.vue';
+
+export const routes = [
+
+    { path: '', component: Home },
+    { path: '/agregadores', component: Agregadores }
+
+];
